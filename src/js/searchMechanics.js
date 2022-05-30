53f1a96856b2eafd995ed0831d87fd4cd9895674
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-05-30 20:54:58 +0300

    axios +

--- src/js/searchMechanics.js
+++ src/js/searchMechanics.js
@@ -1,6 +1,7 @@
 import { refs } from './refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import * as axios from 'axios';
+console.log(axios);
 export const searchMechanics = {
   API_PRIVAT_KEY: '27649790-7921965d78458e948654f4c92',
   page: 1,
@@ -9,8 +10,6 @@ export const searchMechanics = {
   lastRespons: undefined,
 
   fetchPhotos: async function (userQuery) {
-    
-
     //do only when user used search buttom
     if (userQuery) {
       this.page = 1;
@@ -23,16 +22,17 @@ export const searchMechanics = {
 
     const urlQuery = `https://pixabay.com/api/?key=${this.API_PRIVAT_KEY}&q=${this.currentQuery}&image_type=photo&orientation=horizontal&=safesearch=true&page=${this.page}&per_page=${this.per_page}`;
     this.page += 1;
-    const response = await fetch(urlQuery);
-    const dataFromBackEnd = await response.json();
+    // const response = await fetch(urlQuery);
+    // const dataFromBackEnd = await response.json();
+    // this.lastRespons = dataFromBackEnd;
 
-    this.lastRespons = dataFromBackEnd;
+    const response = await axios.get(urlQuery);
+    this.lastRespons = response.data;
 
-    return dataFromBackEnd;
+    return response.data;
   },
 
   checkRechedEnd: function () {
-    
     if (this.per_page * this.page < this.lastRespons.total) return;
     console.log('THE END');
     refs.buttonLoadMore.disabled = true;
